0ba15fa27e910d4f27be61e308d451a3c10aa8d3
tburt <theo@theoburt.com>
2025-02-22 00:03:47 +0000

    added 1st big game (published)

--- docs/js/game_player.js
+++ docs/js/game_player.js
@@ -4,6 +4,7 @@ class CYOAPlayer {
     constructor(container_div, hide_choices_prompt) {
         this.container = container_div;
         this.hide_choices_prompt = hide_choices_prompt;
+        this.hide_entry_ids = true;
         this.game = null
     }
 
@@ -23,12 +24,12 @@ class CYOAPlayer {
         this.entries_container = $('<div>');
 
         const this_player = this;
-        this.play_button.on('click', function(){ 
+        this.play_button.on('click', function () {
             this_player.entries_container.empty();
             this_player.play_button.text('RESTART GAME')
             this_player.begin_game();
         });
-        
+
         this.area_container.append(this.play_button);
         this.area_container.append(num_entries);
         this.area_container.append(this.entries_container);
@@ -36,40 +37,40 @@ class CYOAPlayer {
         this.raw_button = $('<div class="game_raw_button">');
         this.raw_button.text('VIEW RAW ML OUTPUT (SPOILERS)');
 
-        const raw_button_action = function(){ 
+        const raw_button_action = function () {
             this_player.raw_button.text("HIDE RAW OUTPUT");
             this_player.raw_button.unbind('click');
-            this_player.raw_button.on('click', function() { 
+            this_player.raw_button.on('click', function () {
                 this_player.raw_output.remove();
                 this_player.raw_button.text('VIEW RAW ML OUTPUT (SPOILERS)');
                 this_player.raw_button.unbind('click');
                 this_player.raw_button.on("click", raw_button_action);
             });
 
-            this_player.raw_output =  $('<div class="raw_output">');
+            this_player.raw_output = $('<div class="raw_output">');
             const rows = [];
             for (const entry_id of Object.keys(this_player.game.entries)) {
                 const entry = this_player.game.entries[entry_id];
                 const this_row = [entry_id, entry.title, entry.content]
-                if (entry.actions.length > 0) {
-                    this_row.push(entry.actions[0].content);
-                    this_row.push(entry.actions[0].entry_id);
+                if (entry.choices.length > 0) {
+                    this_row.push(entry.choices[0].content);
+                    this_row.push(entry.choices[0].entry_id);
                 }
                 else {
                     this_row.push("");
                     this_row.push("");
                 }
-                if (entry.actions.length > 1) {
-                    this_row.push(entry.actions[1].content);
-                    this_row.push(entry.actions[1].entry_id);
+                if (entry.choices.length > 1) {
+                    this_row.push(entry.choices[1].content);
+                    this_row.push(entry.choices[1].entry_id);
                 }
                 else {
                     this_row.push("");
                     this_row.push("");
                 }
-                if (entry.actions.length > 2) {
-                    this_row.push(entry.actions[2].content);
-                    this_row.push(entry.actions[2].entry_id);
+                if (entry.choices.length > 2) {
+                    this_row.push(entry.choices[2].content);
+                    this_row.push(entry.choices[2].entry_id);
                 }
                 else {
                     this_row.push("");
@@ -117,7 +118,7 @@ class CYOAPlayer {
             //     this.area_container.append(game_desc);
             // }
 
-            this.print_entry("1")
+            this.print_entry(this.game.start_entry_id);
         }
     }
 
@@ -126,38 +127,49 @@ class CYOAPlayer {
 
         var entry_data = this.game.entries[entry_id];
 
-        var entry_id_display = $('<div class="entry_id">');
-        entry_id_display.text(entry_id);
-        entry_div.append(entry_id_display);
+        if (!this.hide_entry_ids) {
+            var entry_id_display = $('<div class="entry_id">');
+            entry_id_display.text(entry_id);
+            entry_div.append(entry_id_display);
+        }
 
-        var entry_title = $('<h2 class="entry_title">');
-        entry_title.text(entry_data.title);
-        entry_div.append(entry_title);
+        if (entry_data.title != null) {
+            var entry_title = $('<h2 class="entry_title">');
+            entry_title.text(entry_data.title);
+            entry_div.append(entry_title);
+        }
 
         var entry_content = $('<div class="entry_content">');
-        entry_content.text(entry_data.content);
+        entry_content.html(entry_data.content.replace(/\n/g, '<br>'));
         entry_div.append(entry_content);
 
         const this_entry = this;
 
-        if (entry_data.actions.length > 0 && !this.hide_choices_prompt) {
+        if (entry_data.choices.length > 0 && !this.hide_choices_prompt) {
             var actions_text = $('<div class="entry_actions_text">');
-            actions_text.text("You may:");
+            actions_text.text("Choices:");
             entry_div.append(actions_text);
         }
 
         const this_entry_div = entry_div[0];
-        for (const action of entry_data.actions) { 
-            var entry_action = $('<div class="entry_action">');
-            entry_action.on('click', function(){
+        for (const choice of entry_data.choices) {
+            var entry_choice = $('<div class="entry_action">');
+            entry_choice.on('click', function () {
                 removeSiblingsAfterl(this_entry_div);
-                this_entry.print_entry(action.entry_id);
+                this_entry.print_entry(choice.entry_id);
             });
-            entry_action.text(action.content + " ["+ action.entry_id +"]");
-            entry_div.append(entry_action);
+
+            if (!this.hide_entry_ids) {
+                entry_choice.text(choice.content + " [" + choice.entry_id + "]");
+            }
+            else {
+                entry_choice.text(choice.content);
+            }
+            
+            entry_div.append(entry_choice);
         }
 
-        if (entry_data.actions.length == 0) {
+        if (entry_data.choices.length == 0) {
             var end_game = $('<div class="end_of_game">');
             end_game.text("END OF GAME");
             entry_div.append(end_game);
@@ -170,13 +182,13 @@ class CYOAPlayer {
 }
 
 function trimAny(str, chars) {
-    var start = 0, 
+    var start = 0,
         end = str.length;
 
-    while(start < end && chars.indexOf(str[start]) >= 0)
+    while (start < end && chars.indexOf(str[start]) >= 0)
         ++start;
 
-    while(end > start && chars.indexOf(str[end - 1]) >= 0)
+    while (end > start && chars.indexOf(str[end - 1]) >= 0)
         --end;
 
     return (start > 0 || end < str.length) ? str.substring(start, end) : str;
@@ -185,14 +197,13 @@ function trimAny(str, chars) {
 function isStringNumeric(str) {
     if (typeof str != "string") return false // we only process strings!  
     return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-           !isNaN(parseInt(str)) // ...and ensure strings of whitespace fail
+        !isNaN(parseInt(str)) // ...and ensure strings of whitespace fail
 }
 
-function removeSiblingsAfterl(dd)
-{
+function removeSiblingsAfterl(dd) {
     var ns;
-    while (ns=dd.nextSibling) 
-    dd.parentNode.removeChild(ns);    
+    while (ns = dd.nextSibling)
+        dd.parentNode.removeChild(ns);
 }
 
 function parse_text_game(text_game) {
@@ -210,6 +221,7 @@ function parse_text_game(text_game) {
     // console.log(rows);
     // return null;
     const entries = {}
+    var first = null;
     for (const row of rows) {
         const entry = {}
         if (row.length > 1) {
@@ -218,58 +230,64 @@ function parse_text_game(text_game) {
         if (row.length > 2) {
             entry.content = row[2];
         }
-        const actions = []
+        const choices = []
         if (row.length > 4) {
-            const action = {}
-            action.content = row[3];
-            action.entry_id = row[4];
+            const choice = {}
+            choice.content = row[3];
+            choice.entry_id = row[4];
 
-            if (isStringNumeric(action.entry_id)) {
-                actions.push(action);
+            if (isStringNumeric(choice.entry_id)) {
+                choices.push(choice);
             }
         }
         if (row.length > 6) {
-            const action = {}
-            action.content = row[5];
-            action.entry_id = row[6];
-            if (isStringNumeric(action.entry_id)) {
-                actions.push(action);
+            const choice = {}
+            choice.content = row[5];
+            choice.entry_id = row[6];
+            if (isStringNumeric(choice.entry_id)) {
+                choices.push(choice);
             }
         }
         if (row.length > 8) {
-            const action = {}
-            action.content = row[7];
-            action.entry_id = row[8];
-            if (isStringNumeric(action.entry_id)) {
-                actions.push(action);
+            const choice = {}
+            choice.content = row[7];
+            choice.entry_id = row[8];
+            if (isStringNumeric(choice.entry_id)) {
+                choices.push(choice);
             }
         }
-        entry.actions = actions
+        entry.choices = choices
         entries[row[0]] = entry
+        if (first == null) {
+            first = row[0];
+        }
     }
     console.log(entries);
-    return { "entries" : entries };
+    return {
+        "entries": entries,
+        "start_entry_id": first
+    };
 }
 
 function createTable(tableData) {
     var table = document.createElement('table');
     var tableBody = document.createElement('tbody');
-  
-    tableData.forEach(function(rowData) {
-      var row = document.createElement('tr');
-  
-      rowData.forEach(function(cellData) {
-        var cell = document.createElement('td');
-        cell.appendChild(document.createTextNode(cellData));
-        row.appendChild(cell);
-      });
-  
-      tableBody.appendChild(row);
+
+    tableData.forEach(function (rowData) {
+        var row = document.createElement('tr');
+
+        rowData.forEach(function (cellData) {
+            var cell = document.createElement('td');
+            cell.appendChild(document.createTextNode(cellData));
+            row.appendChild(cell);
+        });
+
+        tableBody.appendChild(row);
     });
-  
+
     table.appendChild(tableBody);
     return table;
-  }
+}
 
 // $(document).ready(function () {
 //     game = new CYOAPlayer($("#content"));
